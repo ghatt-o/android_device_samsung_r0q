91e7c2e67b543b2c97b80041be31980b93d950b5
Your Name <you@example.com>
2025-03-29 20:44:04 -0300

    first commit

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/twrp_r0q.mk
+
+COMMON_LUNCH_CHOICES := \
+    twrp_r0q-user \
+    twrp_r0q-userdebug \
+    twrp_r0q-eng
