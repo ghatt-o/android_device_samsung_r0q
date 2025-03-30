32989aa04ff07ae52d79d4590cf4e08a47138761
Your Name <you@example.com>
2025-03-30 11:07:15 -0300

    fix the recovery image being 16 bytes bigger than the recovery partition

--- bootimg.mk
+++ bootimg.mk
@@ -12,7 +12,6 @@ $(INSTALLED_RECOVERYIMAGE_TARGET): $(MKBOOTIMG) $(recovery_ramdisk) $(recovery_k
 	@echo "----- Making recovery image ------"
 	$(hide) $(MKBOOTIMG) $(INTERNAL_RECOVERYIMAGE_ARGS) $(INTERNAL_MKBOOTIMG_VERSION_ARGS) $(BOARD_MKBOOTIMG_ARGS) --output $@
 	@echo "----- Lying about SEAndroid state to Samsung bootloader ------"
-	$(hide) echo -n "SEANDROIDENFORCE" >> $@
 	@echo "Made recovery image: $@"
 	$(hide) tar -C $(PRODUCT_OUT) -c recovery.img > $(FLASH_IMAGE_TARGET)
 	@echo "Made flashable $(FLASH_IMAGE_TARGET): $@"
